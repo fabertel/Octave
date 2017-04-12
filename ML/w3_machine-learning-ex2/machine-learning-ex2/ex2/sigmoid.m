6929e4aed42e34b31daeb8affa57c56690ba45f1
Fabio B <fabio.bertellotti@gmail.com>
2017-04-12 12:06:18 +0200

    Add files via upload

--- ML/w3_machine-learning-ex2/machine-learning-ex2/ex2/sigmoid.m
+++ ML/w3_machine-learning-ex2/machine-learning-ex2/ex2/sigmoid.m
@@ -1,6 +1,6 @@
 function g = sigmoid(z)
-%SIGMOID Compute sigmoid function
-%   g = SIGMOID(z) computes the sigmoid of z.
+%SIGMOID Compute sigmoid functoon
+%   J = SIGMOID(z) computes the sigmoid of z.
 
 % You need to return the following variables correctly 
 g = zeros(size(z));
@@ -9,7 +9,8 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
-g = 1 ./ (1 + e.^-z);
+
+g = 1.0 ./ ( 1.0 + e.^(-z)); 
 
 
 % =============================================================
